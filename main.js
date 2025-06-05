e6706938ed5f25d4b0f5e948935d23e84c64ea8e
ikrom.alfurqon7 <ikrom.alfurqon7@gmail.com>
2025-06-05 07:42:42 +0000

    ver1

--- main.js
+++ main.js
@@ -36,3 +36,41 @@ window.addEventListener('click', (e) => {
     }
   });
 });
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".navbar-button");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 100;
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= sectionTop && pageYOffset < sectionTop + sectionHeight) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href").includes(current)) {
+      link.classList.add("active");
+    }
+  });
+});
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  { threshold: 0.2 }
+);
+
+document.querySelectorAll('.fade-in-on-scroll').forEach(el => {
+  observer.observe(el);
+});
